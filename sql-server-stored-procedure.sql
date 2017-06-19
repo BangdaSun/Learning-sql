41105caea6ce9ef1286de2ed33ecb0a24caf2a2f
Bangda Sun <bangdasun94@gmail.com>
2017-06-19 16:03:21 -0400

    Create sql-server-stored-procedure.sql

---
+++ sql-server-stored-procedure.sql
@@ -0,0 +1,19 @@
+/*
+use dbname
+go
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+create/alter procedure myproc (
+  @input1 datatype,
+  @input2 datatype,
+  ...
+) as 
+begin
+
+  [queries]
+
+end
+*/
