0a293e6260d59c584ad5376927602ff286e19a57
Bangda Sun <bangdasun94@gmail.com>
2017-08-16 17:00:57 -0400

    Update sql-server-cte.sql

--- sql-server-cte.sql
+++ sql-server-cte.sql
@@ -1,22 +1,22 @@
 /*
-	CTE - common table expression
-	only supported by sql server
-	6/9/2017
+  CTE - common table expression
+  only supported by sql server
+  6/9/2017
 	
-	Two types: recursive and non-recursive
+  Two types: recursive and non-recursive
 	
-	How to define CTEs?
-	adding a WITH() clause directly before statements like 
-	SELECT, INSERT, UPDATE, DELETE, CREATE VIEW etc
+  How to define CTEs?
+  adding a WITH() clause directly before statements like 
+  SELECT, INSERT, UPDATE, DELETE, CREATE VIEW etc
 	
-	If more than one CTE is in the with, comma is needed to separate
-	For each CTE:
-		1. name
-		2. AS clause
-		3. SELECT clause
-		4. column names (separated by comma)
+  If more than one CTE is in the with, comma is needed to separate
+  For each CTE:
+  1. name
+  2. AS clause
+  3. SELECT clause
+  4. column names (separated by comma)
 		
-	https://www.simple-talk.com/sql/t-sql-programming/sql-server-cte-basics/
+  https://www.simple-talk.com/sql/t-sql-programming/sql-server-cte-basics/
   https://stackoverflow.com/questions/5177346/what-is-the-advantage-of-common-table-expression-in-sql-server
 */
 
@@ -24,21 +24,21 @@
 with cteExample (cte_optId, cte_bid, cte_ask, cte_close_)
 as
 (
-	select optId, bid, ask, close_
-	from optPrice
-	where 1 = 1
-		and optId between 3 and 6
+  select optId, bid, ask, close_
+  from optPrice
+  where 1 = 1
+    and optId between 3 and 6
 )
 select * 
 from cteExample ctee join optPrice op on ctee.cte_optId = op.optId 
 -- the above line must run together with the WITH() clause
 
 /*
-	After we define a CTE with WITH() clause, we can refer the CTE.
-	However, we can refere a CTE only within the execution of the 
-	statement that immediately follows the WITH() clause.
-	After we've run the statement, the CTE result set is not available
-	to other statements. 
+  After we define a CTE with WITH() clause, we can refer the CTE.
+  However, we can refere a CTE only within the execution of the 
+  statement that immediately follows the WITH() clause.
+  After we've run the statement, the CTE result set is not available
+  to other statements. 
 */
 select * from cteExample -- this query won't work
 
@@ -50,21 +50,21 @@ declare @endDate date
 set @startDate = '20170605'
 set @endDate = '20170616'
 ;with cte as (
-	select @startDate as date_
-	union all
-	select DATEADD(DAY, 1, date_) as date_
-	from cte where date_ < @endDate
+  select @startDate as date_
+  union all
+  select DATEADD(DAY, 1, date_) as date_
+  from cte where date_ < @endDate
 )
 select cte.date_
 from cte
 
 -- previous example with non-recursive cte
 ;with cteRange(dateRange) as (
-		select @startDate
-		union all
-		select DATEADD(DAY, 1, dateRange)
-		from cteRange
-		where dateRange <= DATEADD(DAY, -1, @endDate)
+  select @startDate
+  union all
+  select DATEADD(DAY, 1, dateRange)
+  from cteRange
+  where dateRange <= DATEADD(DAY, -1, @endDate)
 )
 select dateRange 
 from cteRange
