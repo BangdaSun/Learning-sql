390d83fe80e3c232de133966c8af7ec47145ecf8
Bangda Sun <bangdasun94@gmail.com>
2017-08-16 17:02:26 -0400

    Update sql-server-manipulate-string.sql

--- sql-server-manipulate-string.sql
+++ sql-server-manipulate-string.sql
@@ -1,8 +1,8 @@
 /*
-	charindex() function
+  charindex() function
   
-	charindex(expressionToFind, expressionToSearch, [start location])
-	return the location of first match
+  charindex(expressionToFind, expressionToSearch, [start location])
+  return the location of first match
 */
 
 declare @target char(3)
@@ -13,8 +13,8 @@ select top 100 CHARINDEX(@target, opraTicker) as isFind
 from ovs.dbo.optContract
 
 declare 
-	@sign char(1),
-	@doc varchar(20)
+  @sign char(1),
+  @doc varchar(20)
 	
 set @sign = '$'
 set @doc = 'I got $x + y = 1$ from $x = 0$ and $y = 1$'
